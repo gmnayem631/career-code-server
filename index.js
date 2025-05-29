11e79f8ab9eb01301b2c5fd43db9edf4c91bf49c
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-05-29 18:09:04 +0600

    initializing backend of career code

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("career code is on its way");
+});
+
+app.listen(port, () => {
+  console.log("Career code is here");
+});
